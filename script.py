9fcba5c016c943769108dda2761eaa0d1df4b63a
Noah Bosshart <noahbosshart@gmail.com>
2023-04-19 13:58:53 +0200

    adding coop downloader and page scroller

--- script.py
+++ script.py
@@ -30,30 +30,75 @@ driver = webdriver.Firefox(service=service, options=firefox_options)
 
 # navigate to the website with the headless browser
 url = input("Please enter Website: ")
-print(f"Downloading from: {url}")
-
-driver.get(url)
-
-# wait for the page to load completely
-time.sleep(3)
-
-# get the page source with requests
-image_elements = driver.find_element(By.CSS_SELECTOR, "div[class*='ng-star-inserted'")
-products = image_elements.find_elements(By.TAG_NAME, "li")
-
-# download the images to the download directory
-for product in products:
-    imgs = product.find_elements(By.TAG_NAME, "img")
-    for i, img in enumerate(imgs):
-        urls_block = str(img.get_attribute("srcset"))
-        urls = re.findall(r'https.*?jpg', urls_block)
-        if urls:
-            url = urls[-1]
-            name = re.findall(r"/([^/]+)\.jpg$", url)[0]
-            response = requests.get(url)
-            with open(f"download_dir/{name}.jpg", "wb") as f:
-                f.write(response.content)
 
 
-# close the browser
-driver.quit()
+def migros(url):
+    
+    driver.get(url)
+
+    # wait for the page to load completely
+    time.sleep(3)
+    
+    height = driver.execute_script("return document.body.scrollHeight")
+    page_range = range(0, height, round(height/10))
+    for i in range(0, len(page_range)-1):
+        driver.execute_script(f"window.scrollTo({page_range[i]}, {page_range[i+1]});")
+    time.sleep(5)
+
+    print(f"Downloading from: Migros")
+    # get the page source with requests
+    image_elements = driver.find_element(By.CSS_SELECTOR, "div[class*='ng-star-inserted'")
+    products = image_elements.find_elements(By.TAG_NAME, "li")
+
+    # download the images to the download directory
+    for product in products:
+        imgs = product.find_elements(By.TAG_NAME, "img")
+        for i, img in enumerate(imgs):
+            urls_block = str(img.get_attribute("srcset"))
+            urls = re.findall(r'https.*?jpg', urls_block)
+            if urls:
+                url = urls[-1]
+                name = re.findall(r"/([^/]+)\.jpg$", url)[0]
+                response = requests.get(url)
+                with open(f"download_dir/{name}.jpg", "wb") as f:
+                    f.write(response.content)
+
+def coop(url):
+    driver.get(url)
+    time.sleep(3)
+    # wait for the page to load completely
+    height = driver.execute_script("return document.body.scrollHeight")
+    page_range = range(0, height, round(height/10))
+    for i in range(0, len(page_range)-1):
+        driver.execute_script(f"window.scrollTo({page_range[i]}, {page_range[i+1]});")
+    time.sleep(5)
+
+    print(f"Downloading from: Coop")
+    # get the page source with requests
+    image_elements = driver.find_element(By.CSS_SELECTOR, "div[class*='list-page back-to-top-offset'")
+    products = image_elements.find_elements(By.TAG_NAME, "li")
+
+    # download the images to the download directory
+    for product in products:
+        imgs = product.find_elements(By.TAG_NAME, "img")
+        for i, img in enumerate(imgs):
+            urls_block = str(img.get_attribute("srcset"))
+            urls = re.findall(r'\/\/.*?\.jpg', urls_block)
+            if urls:
+                url = "https:" + urls[-1]
+                print(url)
+                name = re.findall(r"/([^/]+)\.jpg$", url)[0]
+                response = requests.get(url)
+                with open(f"download_dir/{name}.jpg", "wb") as f:
+                    f.write(response.content)
+
+
+def check_supermarket(url):
+    if "coop" in url:
+        coop(url)
+        driver.quit()
+    else:
+        migros(url)
+        driver.quit()
+
+check_supermarket(url)
